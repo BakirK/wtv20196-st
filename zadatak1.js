2c89422a771f30de83d89147952b8380368a6210
Bakir Karovic <bakir.karovic@gmail.com>
2019-12-15 23:40:20 +0100

    Cetvrti uradjen i pola treceg

--- zadatak1.js
+++ zadatak1.js
@@ -2,16 +2,22 @@ var http = require('http');
 const fs = require('fs');
 const csvjson = require('csvjson');
 const url = require('url');
+
 http.createServer(function (req, res) {
     if (req.method =='GET') {
 		if(req.url == '/') {
+			console.log('got it');
 			fs.readFile('imenik.txt', (err, data) => {
 	    	if(err) {
 	    		console.log(err);
 	    		throw err;	
 	    	} 
 	    	let jsonObj = csvjson.toObject(data.toString());
+	    	res.writeHead('Access-Control-Allow-Origin', 'http://localhost:8080');
 	    	res.writeHead(200, {'Content-Type': 'application/json'});
+	    	//res.writeHead("Access-Control-Allow-Origin", "*");
+		    //res.writeHead("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		    //next();
 	    	res.end(JSON.stringify(jsonObj));
 	    });
 	    } else if(req.url.substr(0, 3) == '/?q') {
@@ -32,7 +38,24 @@ http.createServer(function (req, res) {
 		    });
 	    }
     }
+    else if(req.method=='POST' && req.url == '/json'){
+        let tijeloZahtjeva = '';
+       	req.on('data',(data) => { tijeloZahtjeva += data; });
+       	req.on('end',() => {
+           //primljen čitav zahtjev
+           var parametri = JSON.parse(tijeloZahtjeva);
+           let novaLinija = '\n' + parametri.Ime + "," + parametri.prezime + "," + parametri.adresa + "," 
+           + parametri.broj_telefona;
+           fs.appendFile('imenik.txt', novaLinija , (err) => {
+               if(err) throw err;
+               console.log("Novi red uspješno dodan!");
+               res.writeHead(200,{});
+               res.end(JSON.stringify(parametri));
+           });
+       });
+    }
     else if(req.method=='POST'){
+		console.log(req.url.substr(6));
         let tijeloZahtjeva = '';
         req.on('data',function(data){
         	tijeloZahtjeva+=data;
@@ -49,7 +72,5 @@ http.createServer(function (req, res) {
                 res.end(parametri.toString());
             });
         });
-    }
-    
-    
+	}
 }).listen(8080);
