fd0e471141cddbc5c15f0b12cc8ea4444f29e311
Bakir Karovic <bakir.karovic@gmail.com>
2019-12-21 19:43:32 +0100

    Implementirano brisanje

--- primjer2.js
+++ primjer2.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
 const csvjson = require('csvjson');
+const url = require('url');
 
 app.use(bodyParser.json());
 
@@ -12,9 +13,9 @@ app.post('/',function(req,res){
   res.format({
     'text/html': function () {
       let tijelo = req.body;
-   let novaLinija = '\n'+tijelo['ime']+','+tijelo['prezime']+
+      let novaLinija = '\n'+tijelo['ime']+','+tijelo['prezime']+
        ','+tijelo['adresa']+','+tijelo['broj_telefona'];
-    fs.appendFile('imenik.txt',novaLinija,function(err){
+      fs.appendFile('imenik.txt',novaLinija,function(err){
       if(err) throw err;
       //res.json({message:'Uspješno dodan red',data:novaLinija});
     });
@@ -51,38 +52,55 @@ app.post('/',function(req,res){
 
 
 
-
-app.post('/:',function(req,res){
+app.post('\/:\w+', function(req,res){
   res.format({
     'text/html': function () {
-      let tijelo = req.body;
-   let novaLinija = '\n'+tijelo['ime']+','+tijelo['prezime']+
-       ','+tijelo['adresa']+','+tijelo['broj_telefona'];
-    fs.appendFile('imenik.txt',novaLinija,function(err){
-      if(err) throw err;
-      //res.json({message:'Uspješno dodan red',data:novaLinija});
-    });
     fs.readFile('imenik.txt', (err, data) => {
       if(err) {
           console.log(err);
           throw err;
       }
       let jsonObj = csvjson.toObject(data.toString());
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write('<table ><tr><th>Ime</th>');
-      res.write('<th>Prezime</th>');
-      res.write('<th>Adresa</th>');
-      res.write('<th>Broj telefona</th></tr>');
-
-      for (var i = 0; i < jsonObj.length; i++) {
-        res.write('<tr><td>'+jsonObj[i].Ime+'</td>');
-        res.write('<td>'+jsonObj[i].prezime+'</td>');
-        res.write('<td>'+jsonObj[i].adresa+'</td>');
-        res.write('<td>'+jsonObj[i]['broj telefona']+'</td>');
-        res.write('</tr>');
+      let ime = req.url.substr(2);
+      //console.log(ime);
+      var filtered = jsonObj.filter(function(data) {
+        return data.Ime != ime;
+      });
+      var podaci='Ime,prezime,adresa,broj telefona\n';
+      for (var i = 0; i < filtered.length; i++) {
+         podaci += filtered[i].Ime+','+filtered[i].prezime+
+       ','+filtered[i].adresa+','+filtered[i]['broj telefona']+'\n';
       }
-      res.write('</table>');
-      res.send();
+      fs.writeFile('imenik.txt', podaci, function(){console.log('Uspjesno obrisano')});
+
+      //citanje novih podataka
+      fs.readFile('imenik.txt', (err, data) => {
+        if(err) {
+            console.log(err);
+            throw err;
+        }
+        let jsonObj = csvjson.toObject(data.toString());
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<table ><tr><th>Ime</th>');
+        res.write('<th>Prezime</th>');
+        res.write('<th>Adresa</th>');
+        res.write('<th>Broj telefona</th>');
+        res.write('<th>Obrisi</th>');
+        res.write('<th>Izmijeni</th></tr>');
+
+        for (var i = 0; i < jsonObj.length; i++) {
+          res.write('<tr><td>'+jsonObj[i].Ime+'</td>');
+          res.write('<td>'+jsonObj[i].prezime+'</td>');
+          res.write('<td>'+jsonObj[i].adresa+'</td>');
+          res.write('<td>'+jsonObj[i]['broj telefona']+'</td>');
+          res.write('<td><form target="_self" method="POST" action="http://localhost:8085/:' + jsonObj[i].Ime +
+           '">' + '<input type="submit" value="Obrisi"/></form></td>');
+          res.write('<td><form><input type="submit" value="Izmijeni"/></form></td>');
+          res.write('</tr>');
+        }
+        res.write('</table>');
+        res.send();
+      });
     });
   },
     'default': function () {
@@ -127,8 +145,8 @@ app.get('/',function(req,res){
         res.write('<td>'+jsonObj[i].prezime+'</td>');
         res.write('<td>'+jsonObj[i].adresa+'</td>');
         res.write('<td>'+jsonObj[i]['broj telefona']+'</td>');
-        res.write('<td><form target="_self" method="POST" action="http://localhost:8085/:'+jsonObj[i].Ime'">'+
-          '<input type="submit" value="Obrisi"/></form></td>');
+        res.write('<td><form target="_self" method="POST" action="http://localhost:8085/:' + jsonObj[i].Ime +
+           '">' + '<input type="submit" value="Obrisi"/></form></td>');
         res.write('<td><form><input type="submit" value="Izmijeni"/></form></td>');
         res.write('</tr>');
       }
