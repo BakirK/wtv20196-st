8b80dab17b7d83c5c5cba1c2c7356f5c70dc816a
Bakir Karovic <bakir.karovic@gmail.com>
2019-12-21 20:34:08 +0100

    updated

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const path = require('path');
 app.get('/index.html',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
-app.use('/css', express.static(path.join(__dirname)));
 app.use(express.static('stranice'));
 app.listen(8085);
