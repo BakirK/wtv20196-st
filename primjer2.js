5561729eef23bc2532402b9b9d1b6c090cca73e5
Bakir Karovic <bakir.karovic@gmail.com>
2019-12-21 19:03:35 +0100

    s

--- primjer2.js
+++ primjer2.js
@@ -12,11 +12,11 @@ app.post('/',function(req,res){
   res.format({
     'text/html': function () {
       let tijelo = req.body;
-   let novaLinija = "\n"+tijelo['ime']+","+tijelo['prezime']+
-       ","+tijelo['adresa']+","+tijelo['broj_telefona'];
+   let novaLinija = '\n'+tijelo['ime']+','+tijelo['prezime']+
+       ','+tijelo['adresa']+','+tijelo['broj_telefona'];
     fs.appendFile('imenik.txt',novaLinija,function(err){
       if(err) throw err;
-      //res.json({message:"Uspješno dodan red",data:novaLinija});
+      //res.json({message:'Uspješno dodan red',data:novaLinija});
     });
     fs.readFile('imenik.txt', (err, data) => {
       if(err) {
@@ -25,19 +25,114 @@ app.post('/',function(req,res){
       }
       let jsonObj = csvjson.toObject(data.toString());
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write("<table ><tr><th>Ime</th>");
-      res.write("<th>Prezime</th>");
-      res.write("<th>Adresa</th>");
-      res.write("<th>Broj telefona</th></tr>");
+      res.write('<table ><tr><th>Ime</th>');
+      res.write('<th>Prezime</th>');
+      res.write('<th>Adresa</th>');
+      res.write('<th>Broj telefona</th></tr>');
 
       for (var i = 0; i < jsonObj.length; i++) {
-        res.write("<tr><td>"+jsonObj[i].Ime+"</td>");
-        res.write("<td>"+jsonObj[i].prezime+"</td>");
-        res.write("<td>"+jsonObj[i].adresa+"</td>");
-        res.write("<td>"+jsonObj[i]['broj telefona']+"</td>");
-        res.write("</tr>");
+        res.write('<tr><td>'+jsonObj[i].Ime+'</td>');
+        res.write('<td>'+jsonObj[i].prezime+'</td>');
+        res.write('<td>'+jsonObj[i].adresa+'</td>');
+        res.write('<td>'+jsonObj[i]['broj telefona']+'</td>');
+        res.write('</tr>');
       }
-      res.write("</table>");
+      res.write('</table>');
+      res.send();
+    });
+  },
+    'default': function () {
+      // log the request and respond with 406
+      res.status(406).send('Not Acceptable')
+    }
+  })
+});
+
+
+
+
+
+app.post('/:',function(req,res){
+  res.format({
+    'text/html': function () {
+      let tijelo = req.body;
+   let novaLinija = '\n'+tijelo['ime']+','+tijelo['prezime']+
+       ','+tijelo['adresa']+','+tijelo['broj_telefona'];
+    fs.appendFile('imenik.txt',novaLinija,function(err){
+      if(err) throw err;
+      //res.json({message:'Uspješno dodan red',data:novaLinija});
+    });
+    fs.readFile('imenik.txt', (err, data) => {
+      if(err) {
+          console.log(err);
+          throw err;
+      }
+      let jsonObj = csvjson.toObject(data.toString());
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<table ><tr><th>Ime</th>');
+      res.write('<th>Prezime</th>');
+      res.write('<th>Adresa</th>');
+      res.write('<th>Broj telefona</th></tr>');
+
+      for (var i = 0; i < jsonObj.length; i++) {
+        res.write('<tr><td>'+jsonObj[i].Ime+'</td>');
+        res.write('<td>'+jsonObj[i].prezime+'</td>');
+        res.write('<td>'+jsonObj[i].adresa+'</td>');
+        res.write('<td>'+jsonObj[i]['broj telefona']+'</td>');
+        res.write('</tr>');
+      }
+      res.write('</table>');
+      res.send();
+    });
+  },
+    'default': function () {
+      // log the request and respond with 406
+      res.status(406).send('Not Acceptable')
+    }
+  })
+});
+
+
+
+
+
+
+
+
+
+
+app.get('/unos',function(req,res){
+    res.sendFile(__dirname+'/forma.html');
+});
+
+app.get('/',function(req,res){
+    res.format({
+    'text/html': function () {
+    fs.readFile('imenik.txt', (err, data) => {
+      if(err) {
+          console.log(err);
+          throw err;
+      }
+      let jsonObj = csvjson.toObject(data.toString());
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<table ><tr><th>Ime</th>');
+      res.write('<th>Prezime</th>');
+      res.write('<th>Adresa</th>');
+      res.write('<th>Broj telefona</th>');
+      res.write('<th>Obrisi</th>');
+      res.write('<th>Izmijeni</th></tr>');
+
+      for (var i = 0; i < jsonObj.length; i++) {
+        res.write('<tr><td>'+jsonObj[i].Ime+'</td>');
+        res.write('<td>'+jsonObj[i].prezime+'</td>');
+        res.write('<td>'+jsonObj[i].adresa+'</td>');
+        res.write('<td>'+jsonObj[i]['broj telefona']+'</td>');
+        res.write('<td><form target="_self" method="POST" action="http://localhost:8085/:'+jsonObj[i].Ime'">'+
+          '<input type="submit" value="Obrisi"/></form></td>');
+        res.write('<td><form><input type="submit" value="Izmijeni"/></form></td>');
+        res.write('</tr>');
+      }
+      res.write('</table>');
       res.send();
     });
   },
@@ -47,13 +142,6 @@ app.post('/',function(req,res){
       res.status(406).send('Not Acceptable')
     }
   })
-   
 });
-
-app.get("/unos",function(req,res){
-    res.sendFile(__dirname+"/forma.html");
-});
-
-
 
 app.listen(8085);
